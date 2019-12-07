626ef6782831dcc9ba4545d011ecadd33d8d8f2e
MateuszWierny <maki0602interia.pl>
2019-12-07 14:33:14 +0100

    Wpis

---
+++ src/main/java/sda/project/microblog/model/Post.java
@@ -0,0 +1,32 @@
+package sda.project.microblog.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_post")
+    private long id;
+    private String content;
+
+    Post() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
